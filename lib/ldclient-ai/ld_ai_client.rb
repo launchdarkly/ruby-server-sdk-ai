c246ead2867acec9e4b16b1ba15806914df65182
jsonbailey <jbailey@launchdarkly.com>
2025-05-30 12:47:07 +0000

    Working to get running tests, still failing

--- lib/ldclient-ai/ld_ai_client.rb
+++ lib/ldclient-ai/ld_ai_client.rb
@@ -8,9 +8,33 @@ module LaunchDarkly
   # Namespace for the LaunchDarkly AI SDK.
   #
   module AI
+    # The LDAIConfigTracker class is used to track AI configuration.
+    class AIConfig
+      attr_reader :enabled, :messages, :variables, :tracker
+
+      def initialize(enabled: false, messages: nil, variables: nil, tracker: nil)
+        @enabled = enabled
+        @messages = messages
+        @variables = variables
+        @tracker = tracker
+      end
+
+      def default
+        AIConfig.new(enabled: false)
+      end
+
+      def to_h
+        {
+          enabled: @enabled || false,
+          messages: @messages,
+          variables: @variables,
+          tracker: @tracker
+        }
+      end
+    end
+
     class LDAIClient
-      attr_reader :logger
-      attr_reader :ld_client
+      attr_reader :logger, :ld_client
 
       def initialize(ld_client)
         raise ArgumentError, 'LDClient instance is required' unless ld_client.is_a?(LaunchDarkly::LDClient)
@@ -19,47 +43,47 @@ module LaunchDarkly
         @logger = LaunchDarkly::AI.default_logger
       end
 
-      # Retrieves a configuration and returns a tracker for monitoring its usage
-      # @param config_key [String] The key of the configuration flag
+      # Retrieves the AIConfig
+      # @param key [String] The key of the configuration flag
       # @param context [LDContext] The context used when evaluating the flag
-      # @return [LDAIConfigTracker] A tracker instance for monitoring the configuration usage
-      def config(config_key, context)
-        result = @ld_client.json_variation(config_key, context, {})
-        unless result.is_a?(Hash)
-          raise ArgumentError, 'Result must be a dictionary'
-        end
-
-        unless result.key?('_ldMeta')
-          raise ArgumentError, 'Result must contain _ldMeta'
-        end
-
-        unless result['_ldMeta'].key?('enabled')
-          raise ArgumentError, 'Result must contain _ldMeta.enabled'
-        end
+      # @param default_value [AIConfig]
+      # @param variables [] Optional variables for rendering messages
+      # @return [AIConfig] An AIConfig instance containing the configuration data
+      def config(key, context, default_value, variables: nil)
+        variation = @ld_client.variation(key, context, default_value.to_h)
 
         # Build variables dictionary for rendering messages
-        variables = {}
-        result.each do |key, value|
-          next if key == '_ldMeta'
-          variables[key] = value
+        all_variables = {}
+
+        if variables.nil?
+          variables.each do |key, value|
+            all_variables[key] = value
+          end
         end
+        all_variables['ldctx'] = context.to_h
 
         # Render messages if they exist
-        if result.key?('messages')
-          result['messages'] = result['messages'].map do |message|
-            if message.key?('content')
-              message['content'] = Mustache.render(message['content'], variables)
-            end
+        messages = nil
+        if variation.key?('messages')
+          messages = variation['messages'].map do |message|
+            message['content'] = Mustache.render(message['content'], variables) if message.key?('content')
             message
           end
         end
 
-        LDAIConfigTracker.new(
+        tracker = LDAIConfigTracker.new(
           ld_client: @ld_client,
           config_key: config_key,
           context: context,
           variation_key: config_key,
           version: 1
+        )
+
+        AIConfig.new(
+          enabled: result['_ldMeta']['enabled'],
+          messages: messages,
+          variables: variables,
+          tracker: tracker
         )
       end
     end
--- spec/ld_ai_client_spec.rb
+++ spec/ld_ai_client_spec.rb
@@ -5,25 +5,31 @@ require 'launchdarkly_server_sdk_ai'
 RSpec.describe LaunchDarkly::AI::LDAIClient do
   let(:ld_client) { instance_double(LaunchDarkly::LDClient) }
   let(:context) { instance_double(LaunchDarkly::LDContext) }
-  let(:client) { described_class.new(ld_client) }
+  before {
+    allow(ld_client).to receive(:is_a?).with(LaunchDarkly::LDClient).and_return(true)
+    allow(context).to receive(:is_a?).with(LaunchDarkly::LDContext).and_return(true)
+  }
 
   describe '#initialize' do
-    it 'raises an error if LDClient is not provided' do
+    it '1.2.1: initializes with a valid LDClient instance' do
+      ai_client = described_class.new(ld_client)
+      expect { ai_client }.not_to raise_error
+      expect(ai_client).to be_a(LaunchDarkly::AI::LDAIClient)
+      expect(ai_client.ld_client).to eq(ld_client)
+      expect(ai_client.logger).to be_a(Logger)
+    end
+
+    it '1.2.2: raises an error if LDClient is not provided' do
       expect { described_class.new(nil) }.to raise_error(ArgumentError, 'LDClient instance is required')
     end
 
-    it 'raises an error if LDClient is not an instance of LaunchDarkly::LDClient' do
+    it '1.2.2: raises an error if LDClient is not an instance of LaunchDarkly::LDClient' do
       expect { described_class.new('not a client') }.to raise_error(ArgumentError, 'LDClient instance is required')
-    end
-
-    it 'initializes with a valid LDClient instance' do
-      expect(client.ld_client).to eq(ld_client)
-      expect(client.logger).to eq(LaunchDarkly::AI.default_logger)
     end
   end
 
   describe '#config' do
-    let(:config_key) { 'test-config' }
+    let(:key) { 'key-123' }
     let(:context) { instance_double(LaunchDarkly::LDContext) }
     let(:config_data) do
       {
@@ -37,52 +43,65 @@ RSpec.describe LaunchDarkly::AI::LDAIClient do
       }
     end
 
-    before do
-      allow(ld_client).to receive(:json_variation).with(config_key, context, {}).and_return(config_data)
-    end
+    it 'The LDAIClient MUST provide a method for retrieving a model config' do
+      ai_client = described_class.new(ld_client)
+      ai_config = LaunchDarkly::AI::AIConfig.new
+      allow(ld_client).to receive(:variation).with(key, context, ai_config.to_h).and_return(config_data)
 
-    it 'returns a tracker with the correct configuration' do
-      tracker = client.config(config_key, context)
-      expect(tracker).to be_a(LaunchDarkly::AI::LDAIConfigTracker)
-      expect(tracker.ld_client).to eq(ld_client)
-      expect(tracker.config_key).to eq(config_key)
-      expect(tracker.context).to eq(context)
-      expect(tracker.variation_key).to eq(config_key)
-      expect(tracker.version).to eq(1)
-    end
-
-    it 'raises an error if the result is not a dictionary' do
-      allow(ld_client).to receive(:json_variation).with(config_key, context, {}).and_return('not a dict')
-      expect { client.config(config_key, context) }.to raise_error(ArgumentError, 'Result must be a dictionary')
-    end
-
-    it 'raises an error if _ldMeta is missing' do
-      allow(ld_client).to receive(:json_variation).with(config_key, context, {}).and_return({})
-      expect { client.config(config_key, context) }.to raise_error(ArgumentError, 'Result must contain _ldMeta')
-    end
-
-    it 'raises an error if _ldMeta.enabled is missing' do
-      allow(ld_client).to receive(:json_variation).with(config_key, context, {}).and_return({ '_ldMeta' => {} })
-      expect { client.config(config_key, context) }.to raise_error(ArgumentError, 'Result must contain _ldMeta.enabled')
-    end
-
-    it 'renders messages with variables' do
-      config_data['messages'] = [
-        { 'role' => 'system', 'content' => 'Hello {{name}}!' }
-      ]
-      config_data['name'] = 'World'
       
-      tracker = client.config(config_key, context)
-      expect(config_data['messages'][0]['content']).to eq('Hello World!')
+      config = ai_client.config(key, context, ai_config)
+      expect(config).to be_a(LaunchDarkly::AI::AIConfig)
+      expect(config.enabled).to be false
     end
 
-    it 'preserves messages without content' do
-      config_data['messages'] = [
-        { 'role' => 'system' }
-      ]
+    # it 'returns a tracker with the correct configuration' do
+    #   ai_client = described_class.new(ld_client)
+    #   tracker = ai_client.config(config_key, context)
+    #   expect(tracker).to be_a(LaunchDarkly::AI::LDAIConfigTracker)
+    #   expect(tracker.ld_client).to eq(ld_client)
+    #   expect(tracker.config_key).to eq(config_key)
+    #   expect(tracker.context).to eq(context)
+    #   expect(tracker.variation_key).to eq(config_key)
+    #   expect(tracker.version).to eq(1)
+    # end
+
+    # it 'raises an error if the result is not a dictionary' do
+    #   ai_client = described_class.new(ld_client)
+    #   allow(ld_client).to receive(:json_variation).with(config_key, context, {}).and_return('not a dict')
+    #   expect { ai_client.config(config_key, context) }.to raise_error(ArgumentError, 'Result must be a dictionary')
+    # end
+
+    # it 'raises an error if _ldMeta is missing' do
+    #   ai_client = described_class.new(ld_client)
+    #   allow(ld_client).to receive(:json_variation).with(config_key, context, {}).and_return({})
+    #   expect { ai_client.config(config_key, context) }.to raise_error(ArgumentError, 'Result must contain _ldMeta')
+    # end
+
+    # it 'raises an error if _ldMeta.enabled is missing' do
+    #   ai_client = described_class.new(ld_client)
+    #   allow(ld_client).to receive(:json_variation).with(config_key, context, {}).and_return({ '_ldMeta' => {} })
+    #   expect { ai_client.config(config_key, context) }.to raise_error(ArgumentError, 'Result must contain _ldMeta.enabled')
+    # end
+
+    # it 'renders messages with variables' do
+    #   ai_client = described_class.new(ld_client)
+    #   config_data['messages'] = [
+    #     { 'role' => 'system', 'content' => 'Hello {{name}}!' }
+    #   ]
+    #   config_data['name'] = 'World'
       
-      tracker = client.config(config_key, context)
-      expect(config_data['messages'][0]).to eq({ 'role' => 'system' })
-    end
+    #   tracker = ai_client.config(config_key, context)
+    #   expect(config_data['messages'][0]['content']).to eq('Hello World!')
+    # end
+
+    # it 'preserves messages without content' do
+    #   ai_client = described_class.new(ld_client)
+    #   config_data['messages'] = [
+    #     { 'role' => 'system' }
+    #   ]
+      
+    #   tracker = ai_client.config(config_key, context)
+    #   expect(config_data['messages'][0]).to eq({ 'role' => 'system' })
+    # end
   end
 end
